6bf1769f30bcede4a4713eafb620fd607cd5a41c
Alan Steremberg <alan.steremberg@gmail.com>
2017-01-11 13:23:04 -0800

    first check in

---
+++ commaai_drive.py
@@ -0,0 +1,107 @@
+import argparse
+import base64
+import json
+
+import cv2
+
+import numpy as np
+import socketio
+import eventlet
+import eventlet.wsgi
+import time
+from PIL import Image
+from PIL import ImageOps
+from flask import Flask, render_template
+from io import BytesIO
+import scipy.misc
+
+from keras.models import model_from_json
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
+from keras.optimizers import Adam
+
+# Fix error with Keras and TensorFlow
+import tensorflow as tf
+tf.python.control_flow_ops = tf
+
+
+sio = socketio.Server()
+app = Flask(__name__)
+model = None
+prev_image_array = None
+
+@sio.on('telemetry')
+def telemetry(sid, data):
+    # The current steering angle of the car
+    steering_angle = data["steering_angle"]
+    # The current throttle of the car
+    throttle = data["throttle"]
+    # The current speed of the car
+    speed = data["speed"]
+    # The current image from the center camera of the car
+    imgString = data["image"]
+    image = Image.open(BytesIO(base64.b64decode(imgString)))
+    image_array = np.asarray(image)
+    print(image_array.shape)
+
+    image_array = image_array[55:135, :, :]
+    mean=0
+    image_array=cv2.copyMakeBorder(image_array, top=55, bottom=25 , left=0, right=0, borderType= cv2.BORDER_CONSTANT, value=[mean,mean,mean] )
+    #cv2.resize(image, (160,320))
+
+    b = image_array[None, :, :, :].transpose(0, 3, 1, 2)
+    print(b.shape)
+
+    transformed_image_array = b
+    #transformed_image_array = image_array[None, :, :, :]
+    # This model currently assumes that the features of the model are just the images. Feel free to change this.
+    print("about to call predict")
+    steering_angle = float(model.predict(transformed_image_array, batch_size=1))
+    steering_angle =  steering_angle * scipy.pi / 180 
+    #steering_angle =  steering_angle * scipy.pi / 180 
+   # steering_angle =  steering_angle / 2
+    print("steering angle"+str(steering_angle))
+    # The driving model currently just outputs a constant throttle. Feel free to edit this.
+    speed = float(speed)
+    throttle = 0.2
+    print(steering_angle, throttle)
+    send_control(steering_angle, throttle)
+
+
+@sio.on('connect')
+def connect(sid, environ):
+    print("connect ", sid)
+    send_control(0, 0)
+
+
+def send_control(steering_angle, throttle):
+    sio.emit("steer", data={
+    'steering_angle': steering_angle.__str__(),
+    'throttle': throttle.__str__()
+    }, skip_sid=True)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Remote Driving')
+    parser.add_argument('model', type=str,
+    help='Path to model definition json. Model weights should be on the same path.')
+    args = parser.parse_args()
+    with open(args.model, 'r') as jfile:
+        # NOTE: if you saved the file by calling json.dump(model.to_json(), ...)
+        # then you will have to call:
+        #
+        model = model_from_json(json.loads(jfile.read()))
+        #
+        # instead.
+        #model = model_from_json(jfile.read())
+
+
+    learning_rate=0.0001
+    model.compile(Adam(lr=learning_rate), "mse")
+    weights_file = args.model.replace('json', 'keras')
+    model.load_weights(weights_file)
+
+    # wrap Flask application with engineio's middleware
+    app = socketio.Middleware(sio, app)
+
+    # deploy as an eventlet WSGI server
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
---
+++ commaai_model.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+"""
+Steering angle prediction model
+"""
+import os
+import argparse
+import json
+import pickle
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.layers import Dense, Dropout, Flatten, Lambda, ELU
+from keras.layers.convolutional import Convolution2D
+from sklearn.model_selection import train_test_split
+import cv2
+
+#
+# open and look at the data we are going to use to train
+#
+
+import numpy as np
+from PIL import Image, ImageEnhance, ImageOps
+import scipy.misc
+
+
+path = '/data1/udacity/simulator/data'
+img_path = path +'/IMG'
+csv_file = path +'/driving_log.csv'
+
+# open the CSV file and loop through each line
+# load the CSV so we can have labels
+csv_data=np.recfromcsv(csv_file, delimiter=',', filling_values=np.nan, case_sensitive=True, deletechars='', replace_space=' ')
+print(csv_data.shape)
+#print(csv_data)
+
+# center, left, right, steering angle, throttle, break, speed
+
+# preprocess the data
+
+X_full_name = []
+y_full_angle= []
+
+for line in csv_data:
+  #print(line)
+  X_full_name.append( path+'/'+line[0].decode('UTF-8').strip())
+  y_full_angle.append(float(line[3]))
+  # flip
+  #X_full_name.append( "flip"+path+'/'+line[0].decode('UTF-8').strip())
+  #y_full_angle.append(float(line[3])*-1)
+  # add the left image
+  #X_full_name.append( path+'/'+line[1].decode('UTF-8').strip())
+  #y_full_angle.append(float(line[3])+0.25)
+  # add the right image
+  #X_full_name.append( path+'/'+line[0].decode('UTF-8').strip())
+  #y_full_angle.append(float(line[3])-0.25)
+  #print(X_full_name)
+ 
+
+#
+#  shuffle the data and split it into a validation set
+# 
+
+#X_train, X_validation, y_train, y_validation = train_test_split(X_full_name, y_full_angle, test_size=0.20, random_state=42)
+X_train, X_validation, y_train, y_validation = train_test_split(X_full_name, y_full_angle, test_size=0.01, random_state=42)
+
+
+print(len(X_full_name)) 
+
+
+def process_image(name):
+   top_crop = 50
+   bottom_crop = 140
+   mean=0
+   if 'flip' == name[0:4]:
+      name = name[4:]
+      image = cv2.imread(name)
+      image = cv2.flip(image, 1)
+   else: 
+      image = cv2.imread(name)
+
+   image = image[top_crop:bottom_crop, :, :]
+   image=cv2.copyMakeBorder(image, top=top_crop, bottom=160-bottom_crop , left=0, right=0, borderType= cv2.BORDER_CONSTANT, value=[mean,mean,mean] )
+
+   return np.array(image)[None, :, :, :].transpose(0, 3, 1, 2)[0]
+ 
+
+def generator(X_items,y_items,batch_size):
+  #print("inside generator")
+  gen_state = 0
+  bs = batch_size
+  while 1:
+    if gen_state > len(X_items):
+      bs = batch_size
+      gen_state = 0
+    if gen_state + batch_size > len(X_items):
+      bs = len(X_items) - gen_state
+      #gen_state=0
+    paths = X_items[gen_state : gen_state + bs]
+    yb = y_items[gen_state : gen_state + bs]
+    #y = [ (y1 * 2 ) for y1 in  yb]
+    y = [ (y1 * 180 / scipy.pi) for y1 in  yb]
+    X =  [process_image(x)  for x in paths]
+    #X =  [np.float32((cv2.imread(x, 1)[None, :, :, :].transpose(0, 3, 1, 2) )) / 255.0 for x in paths]
+    gen_state = gen_state + batch_size 
+    #print(len(X))
+    #print(X[0][0].shape)
+    #print(np.asarray(X).shape)
+    #print(np.asarray(X[0]).shape)
+    yield np.asarray(X), np.asarray(y)
+
+def get_model(time_len=1):
+  ch, row, col = 3, 160, 320  # camera format
+
+  model = Sequential()
+  model.add(Lambda(lambda x: x/127.5 - 1.,
+            input_shape=(ch, row, col),
+            output_shape=(ch, row, col)))
+  model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
+  model.add(ELU())
+  model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode="same"))
+  model.add(ELU())
+  model.add(Convolution2D(64, 5, 5, subsample=(2, 2), border_mode="same"))
+  model.add(Flatten())
+  model.add(Dropout(.2))
+  model.add(ELU())
+  model.add(Dense(512))
+  model.add(Dropout(.5))
+  model.add(ELU())
+  model.add(Dense(1))
+
+  #model.load_weights("./outputs/steering_model/steering_angle.keras" )
+
+  #learning_rate=0.00075
+  #learning_rate=0.001
+  learning_rate=0.0015
+  #learning_rate=0.003
+  model.compile(optimizer=Adam(lr=learning_rate), loss="mse")
+
+  return model
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description='Steering angle model trainer')
+  parser.add_argument('--host', type=str, default="localhost", help='Data server ip address.')
+  parser.add_argument('--port', type=int, default=5557, help='Port of server.')
+  parser.add_argument('--val_port', type=int, default=5556, help='Port of server for validation dataset.')
+  parser.add_argument('--batch', type=int, default=64, help='Batch size.')
+  parser.add_argument('--epoch', type=int, default=200, help='Number of epochs.')
+  parser.add_argument('--epochsize', type=int, default=10000, help='How many frames per epoch.')
+  parser.add_argument('--skipvalidate', dest='skipvalidate', action='store_true', help='Multiple path output.')
+  parser.set_defaults(skipvalidate=False)
+  parser.set_defaults(loadweights=False)
+  args = parser.parse_args()
+
+ 
+  print(args.batch)
+
+  model = get_model()
+  res=model.fit_generator(
+    generator(X_train,y_train,args.batch),
+    samples_per_epoch=len(X_train),
+    nb_epoch=args.epoch,
+    validation_data=generator(X_validation,y_validation,args.batch),
+    nb_val_samples=len(X_validation)
+  )
+  print("Saving model weights and configuration file.")
+
+  if not os.path.exists("./outputs/steering_model"):
+      os.makedirs("./outputs/steering_model")
+
+  model.save_weights("./outputs/steering_model/steering_angle.keras", True)
+  with open('./outputs/steering_model/steering_angle.json', 'w') as outfile:
+    json.dump(model.to_json(), outfile)
+
+
+  history=res.history
+  with open('history.p','wb') as f:
+     pickle.dump(history,f)
+
